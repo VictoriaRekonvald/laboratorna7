59943b5612a5646e0ec04a62b2287ae1da004b3c
VictoriaRekonvald <VictoriaRekonvald@gmail.com>
2021-11-04 22:33:37 +0200

    new_br04_11

---
+++ laba1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "windows.h"
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int m;
+    const float g = 9.8;
+    const int p = 1030;
+    double v;
+    printf("input m\n");
+    scanf_s("%i", &m);
+
+    v = m * g / p * g;
+
+    printf("m=%i\nОб'єм зануреної частини судна = V = m * g / p * g = %lf\n", m, g, p, v);
+    return 0;
+}
